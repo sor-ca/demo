a85008ef1a19dbc7c3bb5580705253baaa7206c7
sor-ca <mariagrandova@gmail.com>
2021-12-25 21:10:24 +0200

    remove file

--- space_age/src/main.rs
+++
@@ -1,8 +0,0 @@
-use space_age::Age;
-use space_age::Planet;
-fn main() {
-    let your_age = Age::new();
-    let age_in_seconds=your_age.in_seconds();
-    let your_planet= Planet::new();
-    let your_planet_age = your_age.planet_age(&your_planet);
-}
