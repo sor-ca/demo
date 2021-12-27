4ac858080adaf676ef052e17a237a5887224049c
sor-ca <mariagrandova@gmail.com>
2021-12-27 22:56:08 +0200

    closures from book

---
+++ closures/src/main.rs
@@ -0,0 +1,13 @@
+use closures;
+fn main() {
+    let simuated_user_value=10;
+    let simulated_random_value=7;
+    closures::generate_workout(simuated_user_value, simulated_random_value);
+}
+
+
+
+
+
+
+
