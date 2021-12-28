da899c9284e696b4b36179749bde136f503ac30e
sor-ca <mariagrandova@gmail.com>
2021-12-28 07:22:38 +0200

    closures with HashMap

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::time::Duration;
+use std::collections::HashMap;
 pub fn generate_workout(intensity:u32, random_number:u32) {
     let mut simulated_expensive_result = Cacher::new(|num| {
     println!("calculating slowly...");
@@ -20,24 +21,54 @@ pub fn generate_workout(intensity:u32, random_number:u32) {
 pub struct Cacher<T>
 where T:Fn(u32)->u32, {
         calculation: T,
-        value: Option<u32>,
+        value:HashMap<u32,u32>,
+        //value: Option<u32>,
     }
 impl<T> Cacher<T>
 where T:Fn(u32)->u32, {
     fn new(calculation: T)-> Cacher<T> {
+        let v: HashMap<u32,u32> =HashMap::new();
         Cacher {
             calculation,
-            value: None,
+            value:v,
+            //value: None,
         }
     }
     fn value(&mut self, arg:u32) -> u32 {
-        match self.value {
-            Some(v)=>v,
-            None=> {
-                let v=(self.calculation)(arg);
-                self.value=Some(v);
-                v
+        for (key, value) in &self.value {
+            if *key==arg {
+                return *value;
             }
         }
+        let v=(self.calculation)(arg);
+        (self.value).insert(arg,v);
+        v
+    }
+
+
+        //match self.value {
+            //Some(v)=>v,
+            //None=> {
+                //let v=(self.calculation)(arg);
+                //self.value=Some(v);
+                //v
+            //}
+        //}
+    //}
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn call_with_different_values() {
+        let mut c = Cacher::new(|a| a);
+
+        let v1 = c.value(1);
+        let v2 = c.value(2);
+
+        assert_eq!(v2, 2);
     }
 }
+
+
